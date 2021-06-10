a92a3f17e771e8fdceae0e97eaebb1743b8b0dcc
samiravdalvi10123 <81456967+samiravdalvi10123@users.noreply.github.com>
2021-06-10 16:13:06 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,26 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.Launcher = Constraint.create(options);
+        World.add(world, this.Launcher);
+    }
+    fly(){
+        this.Launcher.bodyA = null;
+    }
+    
+
+    display(){
+       
+        var pointA = this.Launcher.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        
+    }
+}
